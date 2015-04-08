4716412570e310b5424b6a07541b5f05e57ec628
Chris P <christopher.parmer@gmail.com>
2015-04-08 10:29:49 -0400

    base template

---
+++ build_files.sh
@@ -0,0 +1,51 @@
+echo -e "---\nname: 2dhistogram\npermalink: python/2dhistogram\nlayout: base\nthumbnail: /images/2dhistogram.png\n---" > 2015-04-08-python-2dhistogram.html
+echo -e "---\nname: 3d line\npermalink: python/3d-line\nlayout: base\nthumbnail: /images/3d-line.png\n---" > 2015-04-08-python-3d-line.html
+echo -e "---\nname: 3d scatter\npermalink: python/3d-scatter\nlayout: base\nthumbnail: /images/3d-scatter.png\n---" > 2015-04-08-python-3d-scatter.html
+echo -e "---\nname: 3d surface\npermalink: python/3d-surface\nlayout: base\nthumbnail: /images/3d-surface.png\n---" > 2015-04-08-python-3d-surface.html
+echo -e "---\nname: area plots\npermalink: python/area-plots\nlayout: base\nthumbnail: /images/area-plots.png\n---" > 2015-04-08-python-area-plots.html
+echo -e "---\nname: area\npermalink: python/area\nlayout: base\nthumbnail: /images/area.png\n---" > 2015-04-08-python-area.html
+echo -e "---\nname: bar charts\npermalink: python/bar-charts\nlayout: base\nthumbnail: /images/bar-charts.png\n---" > 2015-04-08-python-bar-charts.html
+echo -e "---\nname: bar\npermalink: python/bar\nlayout: base\nthumbnail: /images/bar.png\n---" > 2015-04-08-python-bar.html
+echo -e "---\nname: box plots\npermalink: python/box-plots\nlayout: base\nthumbnail: /images/box-plots.png\n---" > 2015-04-08-python-box-plots.html
+echo -e "---\nname: box\npermalink: python/box\nlayout: base\nthumbnail: /images/box.png\n---" > 2015-04-08-python-box.html
+echo -e "---\nname: bubble\npermalink: python/bubble\nlayout: base\nthumbnail: /images/bubble.png\n---" > 2015-04-08-python-bubble.html
+echo -e "---\nname: bubblecharts\npermalink: python/bubblecharts\nlayout: base\nthumbnail: /images/bubblecharts.png\n---" > 2015-04-08-python-bubblecharts.html
+echo -e "---\nname: contour plots alternate syntax\npermalink: python/contour-plots-alternate-syntax\nlayout: base\nthumbnail: /images/contour-plots-alternate-syntax.png\n---" > 2015-04-08-python-contour-plots-alternate-syntax.html
+echo -e "---\nname: contour plots\npermalink: python/contour-plots\nlayout: base\nthumbnail: /images/contour-plots.png\n---" > 2015-04-08-python-contour-plots.html
+echo -e "---\nname: contour\npermalink: python/contour\nlayout: base\nthumbnail: /images/contour.png\n---" > 2015-04-08-python-contour.html
+echo -e "---\nname: date time\npermalink: python/date-time\nlayout: base\nthumbnail: /images/date-time.png\n---" > 2015-04-08-python-date-time.html
+echo -e "---\nname: error bar\npermalink: python/error-bar\nlayout: base\nthumbnail: /images/error-bar.png\n---" > 2015-04-08-python-error-bar.html
+echo -e "---\nname: error bars alternate syntax\npermalink: python/error-bars-alternate-syntax\nlayout: base\nthumbnail: /images/error-bars-alternate-syntax.png\n---" > 2015-04-08-python-error-bars-alternate-syntax.html
+echo -e "---\nname: error bars\npermalink: python/error-bars\nlayout: base\nthumbnail: /images/error-bars.png\n---" > 2015-04-08-python-error-bars.html
+echo -e "---\nname: facets\npermalink: python/facets\nlayout: base\nthumbnail: /images/facets.png\n---" > 2015-04-08-python-facets.html
+echo -e "---\nname: functions\npermalink: python/functions\nlayout: base\nthumbnail: /images/functions.png\n---" > 2015-04-08-python-functions.html
+echo -e "---\nname: geom polygon\npermalink: python/geom_polygon\nlayout: base\nthumbnail: /images/geom_polygon.png\n---" > 2015-04-08-python-geom_polygon.html
+echo -e "---\nname: gg themes\npermalink: python/gg-themes\nlayout: base\nthumbnail: /images/gg-themes.png\n---" > 2015-04-08-python-gg-themes.html
+echo -e "---\nname: heatmap\npermalink: python/heatmap\nlayout: base\nthumbnail: /images/heatmap.png\n---" > 2015-04-08-python-heatmap.html
+echo -e "---\nname: histogram\npermalink: python/histogram\nlayout: base\nthumbnail: /images/histogram.png\n---" > 2015-04-08-python-histogram.html
+echo -e "---\nname: histogram2d\npermalink: python/histogram2d\nlayout: base\nthumbnail: /images/histogram2d.png\n---" > 2015-04-08-python-histogram2d.html
+echo -e "---\nname: insets\npermalink: python/insets\nlayout: base\nthumbnail: /images/insets.png\n---" > 2015-04-08-python-insets.html
+echo -e "---\nname: line and scatter\npermalink: python/line_and_scatter\nlayout: base\nthumbnail: /images/line_and_scatter.png\n---" > 2015-04-08-python-line_and_scatter.html
+echo -e "---\nname: line exclusive\npermalink: python/line_exclusive\nlayout: base\nthumbnail: /images/line_exclusive.png\n---" > 2015-04-08-python-line_exclusive.html
+echo -e "---\nname: line shapes\npermalink: python/line_shapes\nlayout: base\nthumbnail: /images/line_shapes.png\n---" > 2015-04-08-python-line_shapes.html
+echo -e "---\nname: line and scatter\npermalink: python/line-and-scatter\nlayout: base\nthumbnail: /images/line-and-scatter.png\n---" > 2015-04-08-python-line-and-scatter.html
+echo -e "---\nname: line and scatter3\npermalink: python/line-and-scatter3\nlayout: base\nthumbnail: /images/line-and-scatter3.png\n---" > 2015-04-08-python-line-and-scatter3.html
+echo -e "---\nname: log log plots\npermalink: python/log-log-plots\nlayout: base\nthumbnail: /images/log-log-plots.png\n---" > 2015-04-08-python-log-log-plots.html
+echo -e "---\nname: log\npermalink: python/log\nlayout: base\nthumbnail: /images/log.png\n---" > 2015-04-08-python-log.html
+echo -e "---\nname: logaxes\npermalink: python/logaxes\nlayout: base\nthumbnail: /images/logaxes.png\n---" > 2015-04-08-python-logaxes.html
+echo -e "---\nname: maps\npermalink: python/maps\nlayout: base\nthumbnail: /images/maps.png\n---" > 2015-04-08-python-maps.html
+echo -e "---\nname: matlab multiple axes\npermalink: python/matlab-multiple-axes\nlayout: base\nthumbnail: /images/matlab-multiple-axes.png\n---" > 2015-04-08-python-matlab-multiple-axes.html
+echo -e "---\nname: mixed types\npermalink: python/mixed-types\nlayout: base\nthumbnail: /images/mixed-types.png\n---" > 2015-04-08-python-mixed-types.html
+echo -e "---\nname: mixed\npermalink: python/mixed\nlayout: base\nthumbnail: /images/mixed.png\n---" > 2015-04-08-python-mixed.html
+echo -e "---\nname: multiple axes\npermalink: python/multiple-axes\nlayout: base\nthumbnail: /images/multiple-axes.png\n---" > 2015-04-08-python-multiple-axes.html
+echo -e "---\nname: network graphs\npermalink: python/network_graphs\nlayout: base\nthumbnail: /images/network_graphs.png\n---" > 2015-04-08-python-network_graphs.html
+echo -e "---\nname: polar\npermalink: python/polar\nlayout: base\nthumbnail: /images/polar.png\n---" > 2015-04-08-python-polar.html
+echo -e "---\nname: scatter exclusive\npermalink: python/scatter_exclusive\nlayout: base\nthumbnail: /images/scatter_exclusive.png\n---" > 2015-04-08-python-scatter_exclusive.html
+echo -e "---\nname: scatter plots alternate syntax\npermalink: python/scatter-plots-alternate-syntax\nlayout: base\nthumbnail: /images/scatter-plots-alternate-syntax.png\n---" > 2015-04-08-python-scatter-plots-alternate-syntax.html
+echo -e "---\nname: sparse matrix graphs\npermalink: python/sparse_matrix_graphs\nlayout: base\nthumbnail: /images/sparse_matrix_graphs.png\n---" > 2015-04-08-python-sparse_matrix_graphs.html
+echo -e "---\nname: streaming subsection placeholder\npermalink: python/streaming-subsection-placeholder\nlayout: base\nthumbnail: /images/streaming-subsection-placeholder.png\n---" > 2015-04-08-python-streaming-subsection-placeholder.html
+echo -e "---\nname: subplots alternate syntax\npermalink: python/subplots-alternate-syntax\nlayout: base\nthumbnail: /images/subplots-alternate-syntax.png\n---" > 2015-04-08-python-subplots-alternate-syntax.html
+echo -e "---\nname: subplots\npermalink: python/subplots\nlayout: base\nthumbnail: /images/subplots.png\n---" > 2015-04-08-python-subplots.html
+echo -e "---\nname: text charts\npermalink: python/text-charts\nlayout: base\nthumbnail: /images/text-charts.png\n---" > 2015-04-08-python-text-charts.html
+echo -e "---\nname: text\npermalink: python/text\nlayout: base\nthumbnail: /images/text.png\n---" > 2015-04-08-python-text.html
+echo -e "---\nname: time series\npermalink: python/time-series\nlayout: base\nthumbnail: /images/time-series.png\n---" > 2015-04-08-python-time-series.html
